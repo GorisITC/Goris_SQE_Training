1ad0e9913d95c0d287c357512eb6f0abf369017e
Anush21 <anush.lalazaryan@gmail.com>
2017-08-02 08:40:51 +0400

    Python's_Homework_Inequation

---
+++ Anush_L/Python's Projects/Homework_3_v0.1.py
@@ -0,0 +1,10 @@
+print "X^2 / 2 > n"
+x = input("Please enter X value: ")
+n = 0
+if type(x) == int:
+	x = x * 1.0
+	n = x * x / 2
+	n = n - 1
+	print "The maximum value of n is: " + str(n)
+else:
+	print "X should be only a number"
