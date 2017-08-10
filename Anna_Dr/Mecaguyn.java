f522db398656904c3977ecf3a418e01e041e15f6
adravants <adravants@mail.ru>
2017-08-10 17:05:20 +0400

    Add files via upload

---
+++ Anna_Dr/Mecaguyn.java
@@ -0,0 +1,29 @@
+public class Mecaguyn {
+	public static void main(String[] args) {
+		int abc = 783;
+		int a = (abc/100);
+		int b = ((abc/10) % 10);
+		int c = (abc%10);
+		float d = (abc/(a+b+c));
+		if (abc>d){
+			System.out.println(d);
+			}
+	    	    else{
+			System.out.println(false);
+			}
+				
+
+
+
+
+
+
+
+
+
+
+
+
+			
+		}
+}
