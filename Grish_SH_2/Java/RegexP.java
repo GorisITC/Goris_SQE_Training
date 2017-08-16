19ca79710976d5ab5683087f1086d3b0affb756e
Grish96 <grisho.shalunts.1996@gmail.com>
2017-08-16 16:11:38 +0400

    Update RegexP.java

--- Grish_SH_2/Java/RegexP.java
+++ Grish_SH_2/Java/RegexP.java
@@ -6,7 +6,7 @@ public class RegexP{
     public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Please enter the text...");
-        String input = sc.next();
+        String input = sc.nextLine();
         String regex = "[A-Za-z0-9]*\\d+[A-z]*";
         Pattern l = Pattern.compile(regex);
         Matcher m = l.matcher(input);
