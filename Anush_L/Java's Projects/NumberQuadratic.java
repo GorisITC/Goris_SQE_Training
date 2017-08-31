fa4941e758ff953b15401990eefebc4fc4afff3b
Anush21 <anush.lalazaryan@gmail.com>
2017-08-31 14:24:03 +0400

    Update NumberQuadratic.java

--- Anush_L/Java's Projects/NumberQuadratic.java
+++ Anush_L/Java's Projects/NumberQuadratic.java
@@ -1,16 +1,19 @@
-
 public class NumberQuadratic {
 	public static void main(String[] args) {
-		int n = 0;
+		int n = 1;
 		int i;
-		int qd = 1;
-		for (i = 2; i < n; i*=2) {
-			qd++;
-		}
-		if (i == n) {
-			System.out.println(n + "֊ը 2-ի " + qd + " աստիճանն է։");
+		int qd = 1; 
+		if (n == 1) {
+			System.out.println(n + "֊ը 2-ի 0 աստիճանն է։");
 		} else {
-			System.out.println(n + "֊ը 2֊ի աստիճան չէ։");
+			for (i = 2; i < n; i*=2) {
+				qd++;
+			}
+			if (i == n) {
+				System.out.println(n + "֊ը 2-ի " + qd + " աստիճանն է։");
+			} else {
+				System.out.println(n + "֊ը 2֊ի աստիճան չէ։");
+			}
 		}
 	}
 }
