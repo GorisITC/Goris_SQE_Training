4b079bd628ab68b18c98ec72b6a8d7634e38785c
Anush21 <anush.lalazaryan@gmail.com>
2017-09-01 07:07:17 +0400

    Homework_04_Simple_Numbers

---
+++ Anush_L/Java's Projects/SimpleNumbers.java
@@ -0,0 +1,20 @@
+public class SimpleNumbers {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 500; i++) {
+			if (i > 2) {
+				int d = 0;
+				for (int j = 2; j < i; j++) {
+					if (i % j == 0) {
+						d++;
+						break;
+					}
+				}
+				if (d < 1) {
+					System.out.println(i);
+				}
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+}
