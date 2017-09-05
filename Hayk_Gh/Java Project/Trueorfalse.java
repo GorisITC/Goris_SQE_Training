9ffdd77b918e9d2f70f7a1d70891feea2f3d72cf
Anush21 <anush.lalazaryan@gmail.com>
2017-09-05 07:04:11 +0400

    Previous_Homeworks

---
+++ Hayk_Gh/Java Project/Trueorfalse.java
@@ -0,0 +1,20 @@
+public class Trueorfalse {
+
+    public static void main (String[] args){
+
+
+                int a = 16;
+
+                boolean b = (a % 2 == 0);
+
+                System.out.println(b);
+
+
+    }
+
+
+}
+
+
+
+
