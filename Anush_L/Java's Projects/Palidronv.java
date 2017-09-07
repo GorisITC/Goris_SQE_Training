47b16bfd8818a9e129547dd95e577c9dd7523a63
Anush21 <anush.lalazaryan@gmail.com>
2017-09-07 21:23:17 +0400

    Polidrom_Version1_Version2

---
+++ Anush_L/Java's Projects/Palidronv.java
@@ -0,0 +1,11 @@
+
+public class Palidronv {
+	public static void main(String[] args) {
+		String a = "Anna";
+		StringBuilder b = new StringBuilder(a);
+		b.reverse();
+		String c = b.toString();
+		System.out.println(a.equalsIgnoreCase(c));
+	}
+
+}
