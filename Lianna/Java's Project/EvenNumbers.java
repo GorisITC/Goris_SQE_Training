02c9ca64be26d1c9c284d7535221cb47ace6e0a4
Lianna-H <31122357+Lianna-H@users.noreply.github.com>
2017-09-08 09:35:16 -0400

    Homework

---
+++ Lianna/Java's Project/EvenNumbers.java
@@ -0,0 +1,16 @@
+
+public class EvenNumbers {
+	public static void main(String[] args) {
+		int [][] a = {{2, 2, 3}, {4, 8, 6}, {7, 8, 4}};
+	        for (int i = 0; i < a.length; i ++) {
+	        	if (a[i][i]%2==0) {
+	        		System.out.println(a[i][i]);
+	           }
+	    }
+	}          
+}
+        
+    
+
+
+
