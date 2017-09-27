e3a34b5fd0851bd1f19ad5ef77d74b52b46526e1
Grish96 <grisho.shalunts.1996@gmail.com>
2017-09-27 23:23:30 +0400

    Homework of 27.09.2017

---
+++ Grish_SH_2/Java/BookClass/src/Book.java
@@ -0,0 +1,69 @@
+public class Book {
+    String title,auther,categories,genre,content;
+    int count,page,pubyear;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuther() {
+        return auther;
+    }
+
+    public void setAuther(String auther) {
+        this.auther = auther;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPubyear() {
+        return pubyear;
+    }
+
+    public void setPubyear(int pubyear) {
+        this.pubyear = pubyear;
+    }
+
+}
--- Grish_SH_2/Java/src/Main.java
+++ Grish_SH_2/Java/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         circle.pointincircle(16,1,1,5,2,3);
         Triangle triangle = new Triangle();
         triangle.areatriangle(4,5,4,6,8,4);
+        triangle.poi
     }
 
 }
