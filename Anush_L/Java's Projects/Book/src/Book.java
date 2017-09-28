55d2cdb2f2640c1d6a5b73a0907ac5cdea1c1da4
Anush21 <anush.lalazaryan@gmail.com>
2017-09-28 08:24:44 +0400

    Homework_Book

---
+++ Anush_L/Java's Projects/Book/src/Book.java
@@ -0,0 +1,54 @@
+
+public class Book {
+	String title, author, genre;
+	int pubYear, count, pages, price;
+	
+	public void setTitle (String title) {
+		this.title = title;
+	}
+	public String getTitle () {
+		return title;
+	}
+	
+	public void setAuthor (String author) {
+		this.author = author;
+	}
+	public String getAuthor () {
+		return author;
+	}
+	
+	public void setGenre (String genre) {
+		this.genre = genre;
+	}
+	public String getGenre () {
+		return genre;
+	}
+	
+	public void setPubYear (int pubYear) {
+		this.pubYear = pubYear;
+	}
+	public int getPubYear () {
+		return pubYear;
+	}
+	
+	public void setCount (int count) {
+		this.count = count;
+	}
+	public int getCount () {
+		return count;
+	}
+	
+	public void setPages (int pages) {
+		this.pages = pages;
+	}
+	public int getPages () {
+		return pages;
+	}
+	
+	public void setPrice (int price) {
+		this.price = price;
+	}
+	public int getPrice () {
+		return price;
+	}
+}
