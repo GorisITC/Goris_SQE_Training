30e29a01d073bb8aece8aa827b6758290f1372cd
Anush21 <anush.lalazaryan@gmail.com>
2017-09-28 13:52:39 +0400

    Homework_Book

---
+++ Hayk_Gh/Java Project/Book/src/Author.java
@@ -0,0 +1,10 @@
+public class Author {
+    private String authorname;
+
+    public void setAuthorname(String authorname){
+        this.authorname = authorname;
+    }
+    public String getAuthorname(){
+        return authorname;
+    }
+}
