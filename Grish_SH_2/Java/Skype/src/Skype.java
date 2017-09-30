59194ad77b0b1a46215cd199f73d8f9507716628
Grish96 <grisho.shalunts.1996@gmail.com>
2017-09-30 21:25:37 +0400

    Homework of 30.09.2017 with a lot of support

---
+++ Grish_SH_2/Java/Skype/src/Skype.java
@@ -0,0 +1,21 @@
+public class Skype {
+    boolean offline;
+    boolean online;
+
+    Message [] emptymessage3 = new Message [10];
+
+    public void OfflineOrOnline){
+        if(offline){
+        Message emptymessage2 = new Message();
+        emptymessage2.setText(null);
+            System.out.println("Please dont bother me");
+        }else{
+
+            for (int i = 0;i<emptymessage3.length;i++){
+
+            }
+
+        }
+    }
+
+}
