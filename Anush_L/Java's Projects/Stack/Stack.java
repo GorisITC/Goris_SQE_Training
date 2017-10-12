679596d4fd12324c5f16907e456563823a61ee4e
Anush21 <anush.lalazaryan@gmail.com>
2017-10-12 10:18:50 +0400

    Homework_Stack_v0.1

--- Anush_L/Java's Projects/Stack/Stack.java
+++ Anush_L/Java's Projects/Stack/Stack.java
@@ -4,24 +4,22 @@ public class Stack {
 	int count = 0;
 	String[] stack;	
 	
+	public Stack() {
+		length = 5;
+		stack = new String[this.length];
+
+	}
+	
 	public Stack(int length) {
-		if (length > 0) {
-			this.length = length;
-			stack = new String[this.length];
-		} else {
-			this.length = 5;
-			stack = new String[this.length];
-			System.out.println("Stack should have length more than 0, so I created your Stack with length 5.");
-		}
+		if (length <= 0) throw new NegativeArraySizeException("Stack should have length more than 0,");
+		this.length = length;
+		stack = new String[this.length];
 	}
 	
 	public void push(String item) {
-		if (isFull() == false) {
-			stack[count] = item;
-			count++;
-		} else {
-			System.out.println("Unfortunately, your Stack is full. You are not able to push new item.");
-		}
+		if (isFull() == true) throw new ArrayIndexOutOfBoundsException("Unfortunately, your Stack is full. You are not able to push new item.");
+		stack[count] = item;
+		count++;
 	}
 	
 	public String pop() {
