cb41e768de2675879f8c0c5e4ffeb7d97cfeaa1b
Grish96 <grisho.shalunts.1996@gmail.com>
2017-11-29 20:28:00 +0400

    Terminal

--- Grish_SH_2/Java/Terminal/Main.java
+++ Grish_SH_2/Java/Terminal/Main.java
@@ -1,23 +1,26 @@
+import sun.management.HotspotMemoryMBean;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String currentPath = "/home/grish/";
+        String currentPath = System.getProperty("user.home");
         Scanner scanner = new Scanner(System.in);
         File file = new File(currentPath);
         while (true) {
             System.out.print(file.getPath() + ":~$ ");
             String command = scanner.nextLine();
-            if (command.contains("cd ")) {
-                if (command.contains("..")) {
+            if (command.startsWith("cd ")) {
+                String fileName = command.replaceAll("cd ","");
+                if (fileName.equals("..")) {
                     file = file.getParentFile();
                 } else {
                     File file1 = new File((file.getPath().endsWith("/") ? file.getPath() : file.getPath() + "/") + command.replaceAll("cd ", "").trim());
                     if (file1.exists() && file1.isDirectory()) {
                         file = file1;
-                    } else System.out.println("--No such file or directory--");
+                    } else System.out.println("--Command not found--");
                     currentPath = file.getPath();
                 }
             }
@@ -27,26 +30,34 @@ public class Main {
             if (command.equals("ls")) {
                 ls(file);
             }
-            if (command.equals("touch")) {
-                touch(file);
+            if (command.startsWith("touch ")) {
+                touch(file,command.replaceAll("touch ",""));
+
+            }
+            if (command.startsWith("mkdir ")) {
+                mkdir(file,command.replaceAll("mkdir ",""));
 
             }
 
         }
 
     }
-
     private static void ls(File file) {
         String[] files = file.list();
         for (int i = 0; i < files.length; i++) {
             System.out.println(files[i]);
         }
     }
-    private static void touch (File file) throws IOException {
-        if(!file.exists()){
-            file.createNewFile();
+    private static void touch (File file,String name) throws IOException {
+        File newFile  =  new File(file.getPath() + "/" + name);
+        if(!newFile.exists()){
+            newFile.createNewFile();
         }
-
     }
-
+    private static void mkdir (File file,String name) throws IOException {
+        File newFile = new File(file.getPath() + "/" + name);
+        if (!newFile.exists()) {
+            newFile.mkdir();
+        }
+    }
 }
