5f94ac5c7af38fce0dff40554e205a1911e87a06
Hayk1997gh <31442337+Hayk1997gh@users.noreply.github.com>
2017-12-06 19:11:52 +0400

    Homework_Terminal_NewVersion
    Added mkdir and touch commands.Divided into logical functions.

--- members/Hayk_Gh/Java Project/Terminal.java
+++ members/Hayk_Gh/Java Project/Terminal.java
@@ -1,38 +1,86 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Terminal {
-    public static void main(String[] args){
-        File current = new File("C:\\");
+
+    static File current = new File(System.getProperty("user.home"));
+    static String command = null;
+
+   static void cd(String command) {
+        if (command.startsWith("cd")) {
+            if (command.contains("..")) {
+                current = new File(String.valueOf(current.getParentFile()));
+            }
+            String string = command.substring(3);
+            File secondFile = new File(current.getPath() + "\\" + string);
+            if (!secondFile.exists()) {
+                System.out.println("No such folder or directory:" + string);
+            } else {
+                current = secondFile;
+            }
+            System.out.println(current.getPath());
+        }
+    }
+
+   static void ls(File file) throws Exception {
+        String[] files = current.list();
+        if(current.isDirectory()) {
+            if (files.length == 0) {
+                System.out.println("Your folder is Empty");
+            }
+            for (int i = 0; i < files.length; i++) {
+                System.out.println(files[i]);
+            }
+        }else {
+            throw new Exception("This is not Directory");
+        }
+    }
+
+    static void pwd(File file) {
         System.out.println(current.getPath());
-        String command = null;
-        while (true){
+    }
+
+    static void mkdir(String command) {
+        if (command.startsWith("mkdir")) {
+            String command1 = command.substring(6);
+            File file = new File(Terminal.current.getPath() + "\\" + command1);
+            file.mkdir();
+            current = file;
+            System.out.println(current.getPath());
+        }
+    }
+
+    static void touch(String command) throws IOException {
+        if (command.startsWith("touch")) {
+            String command1 = command.substring(6);
+            File file = new File(Terminal.current.getPath() + "\\" + command1);
+            file.createNewFile();
+            current = file;
+            System.out.println(current.getPath());
+        }
+    }
+
+   public static void main(String[] args) throws Exception {
+        System.out.println(current.getPath());
+        while (true) {
             Scanner scanner = new Scanner(System.in);
             command = scanner.nextLine();
-            if(command.startsWith("cd")){
-                if(command.contains("..")){
-                    current = new File(String.valueOf(current.getParentFile()));
-                }   String string = command.substring(3);
-                    File secondFile = new File(current.getPath() + "\\" + string);
-                    if (!secondFile.exists() || !secondFile.isDirectory()) System.out.println("No such file or directory:" + string );
-                    else current = secondFile;
-                    System.out.println(current.getPath());
-            }else if(command.equals("ls")){
-                String [] files = current.list();
-                if(files.length==0){
-                    System.out.println("Your folder is Empty");
-                }
-                for (int i = 0;i<files.length;i++){
-                    System.out.println(files[i]);
-                }
-            }else if(command.equals("pwd")){
-                System.out.println(current.getPath());
-            }else {
-                System.out.println("Please input valid command");
+            if (command.equals("ls")) {
+                ls(current);
+            } else if (command.startsWith("cd")) {
+                cd(command);
+            } else if (command.equals("pwd")) {
+                pwd(current);
+            } else if (command.startsWith("mkdir")) {
+                mkdir(command);
+            } else if (command.startsWith("touch")) {
+                touch(command);
             }
         }
     }
 }
+
 
 
 
