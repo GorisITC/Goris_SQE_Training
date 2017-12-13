a774492d0d1561b049f983b5adfe10e592475ce3
ArusAghabekyan <arus.aghabekian@gmail.com>
2017-12-13 10:52:39 +0400

    doneTerminalOrComandLine

--- members/Arus_A/JavaProject/CommandLine/Terminal.java
+++ members/Arus_A/JavaProject/CommandLine/Terminal.java
@@ -3,40 +3,22 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Terminal {
+    Scanner userInput = new Scanner(System.in);
+    String fullPath = System.getProperty("user.home");
+    String command = userInput.nextLine();
+    String[] path;
+    File f1 = new File(fullPath);
+    String[] newFileName;
+    boolean exit = false, back = false;
 
     public void run() {
-        Scanner userInput = new Scanner(System.in);
-        String fullPath = System.getProperty("user.home");
-        String command = userInput.nextLine();
-        String[] path;
-        File f1 = new File(fullPath);
-        String[] newFileName;
-        boolean exit = false, back = false;
-
         while (exit != true) {
             String[] exactCommand = command.split(" ");
             if (exactCommand[0].equals("cd")) {
                 Commands com = Commands.cd;
                 try {
                     if (com == Commands.cd) {
-                        path = command.split(" ");
-                        if (!path[1].startsWith("..")) {
-                            fullPath = f1.getPath() + "/" + path[1] + "/";
-                            f1 = new File(fullPath);
-                            if (f1.exists() && f1.isDirectory()) {
-                                System.out.println(fullPath);
-                            } else {
-                                f1 = f1.getParentFile();
-                                System.out.println("no such file or directory");
-                            }
-                        } else if (path[1].endsWith("..")) {
-                            f1 = f1.getParentFile();
-                            System.out.println(f1);
-
-                            back = true;
-                        } else {
-                            System.out.println("no such file or directory");
-                        }
+                        cd();
                     }
                     command = userInput.nextLine();
                 } catch (ArrayIndexOutOfBoundsException e) {
@@ -45,25 +27,14 @@ public class Terminal {
                 }
             } else if (command.equals("pwd")) {
                 Commands com = Commands.pwd;
-                if (com == Commands.pwd && back == false) {
-                    System.out.println(f1);
-                    command = userInput.nextLine();
-                } else if (back == true) {
-                    System.out.println(f1);
-                    command = userInput.nextLine();
+                if (com == Commands.pwd) {
+                    pwd();
                 }
             } else if (command.equals("ls")) {
                 Commands com = Commands.ls;
                 try {
                     if (com == Commands.ls) {
-                        String[] files = f1.list();
-                        for (int i = 0; i < files.length; i++) {
-                            if (files[i].startsWith(".")) {
-                                continue;
-                            } else {
-                                System.out.println(files[i]);
-                            }
-                        }
+                        ls();
                     } else {
                         System.out.println("ls: cannot access " + "'" + exactCommand[1] + "':" + " No such file or directory");
                     }
@@ -75,16 +46,7 @@ public class Terminal {
                 Commands com = Commands.mkdir;
                 try {
                     if (com == Commands.mkdir) {
-
-                        newFileName = command.split(" ");
-                        File newFile = new File(f1.getPath() + "/" + newFileName[1] + "/");
-                        if (!newFile.exists()) {
-                            newFile.mkdir();
-                            f1 = newFile;
-                        } else if (newFile.exists()) {
-                            System.out.println("cannot create directory " + "'" + newFileName[1] + "':" + " File exists");
-                        }
-                        command = userInput.nextLine();
+                        mkdir();
                     }
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println("mkdir: missing operand");
@@ -93,22 +55,9 @@ public class Terminal {
             } else if (exactCommand[0].equals("touch")) {
                 try {
                     Commands com = Commands.touch;
-                    try {
-                        if (com == Commands.touch) {
 
-                            newFileName = command.split(" ");
-                            File newFile = new File(f1.getPath() + "/" + newFileName[1] + "/");
-                            if (!newFile.exists()) {
-                                newFile.createNewFile();
-                                f1 = newFile;
-                            } else if (newFile.exists()) {
-                                System.out.println("cannot create directory " + "'" + newFileName[1] + "':" + " File exists");
-                            }
-                            command = userInput.nextLine();
-                        }
-                    } catch (IOException e) {
-                        System.out.println("touch: missing file operand");
-                        command = userInput.nextLine();
+                    if (com == Commands.touch) {
+                        touch();
                     }
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println("touch: missing file operand");
@@ -117,7 +66,7 @@ public class Terminal {
             } else if (command.equals("exit")) {
                 Commands com = Commands.exit;
                 if (com == Commands.exit) {
-                    exit = true;
+                    close();
                 }
             } else {
                 System.out.println("command not found");
@@ -125,4 +74,80 @@ public class Terminal {
             }
         }
     }
+
+    public void cd() {
+        path = command.split(" ");
+        if (!path[1].startsWith("..")) {
+            fullPath = f1.getPath() + "/" + path[1] + "/";
+            f1 = new File(fullPath);
+            if (f1.exists() && f1.isDirectory()) {
+                System.out.println(fullPath);
+            } else {
+                f1 = f1.getParentFile();
+                System.out.println("no such file or directory");
+            }
+        } else if (path[1].endsWith("..")) {
+            f1 = f1.getParentFile();
+            System.out.println(f1);
+
+            back = true;
+        } else {
+            System.out.println("no such file or directory");
+        }
+    }
+
+    public void pwd() {
+        if (back == false) {
+            System.out.println(f1);
+            command = userInput.nextLine();
+        } else if (back == true) {
+            System.out.println(f1);
+            command = userInput.nextLine();
+        }
+    }
+
+    public void mkdir() {
+        newFileName = command.split(" ");
+        File newFile = new File(f1.getPath() + "/" + newFileName[1] + "/");
+        if (!newFile.exists()) {
+            newFile.mkdir();
+            f1 = newFile;
+        } else if (newFile.exists()) {
+            System.out.println("cannot create directory " + "'" + newFileName[1] + "':" + " File exists");
+        }
+        command = userInput.nextLine();
+    }
+
+    public void ls() {
+        String[] files = f1.list();
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].startsWith(".")) {
+                continue;
+            } else {
+                System.out.println(files[i]);
+            }
+        }
+    }
+
+    public void touch() {
+        try {
+            newFileName = command.split(" ");
+            File newFile = new File(f1.getPath() + "/" + newFileName[1] + "/");
+            if (!newFile.exists()) {
+                newFile.createNewFile();
+                f1 = newFile;
+            } else if (newFile.exists()) {
+                System.out.println("cannot create directory " + "'" + newFileName[1] + "':" + " File exists");
+            }
+            command = userInput.nextLine();
+        } catch (IOException e) {
+            System.out.println("touch: missing file operand");
+            command = userInput.nextLine();
+        }
+    }
+
+    public void close() {
+        exit = true;
+    }
+
 }
